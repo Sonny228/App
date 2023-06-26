010f1bbe5897ed897b3b48d5cd5f11e232b809ab
Sonny228 <137756948+Sonny228@users.noreply.github.com>
2023-06-26 17:18:14 +1000

    Initial commit

---
+++ taskmanager/main/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+from . import forms,models
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks' : tasks})
+
+def akk(request):
+    return render(request, 'main/akk.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Неверная форма'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+def search_view(request):
+    # whatever user write in search box we get in query
+    query = request.GET['query']
+    products=models.Product.objects.all().filter(name__icontains=query)
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart = 0
